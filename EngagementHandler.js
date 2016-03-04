d1c4b9317f7b87e2e4738a9a5539e630dd1363b7
waruna <waruna@duosoftware.com>
2016-03-04 16:03:15 +0530

    after load test

--- EngagementHandler.js
+++ EngagementHandler.js
@@ -17,10 +17,12 @@ var MongoClient = require('mongodb').MongoClient
 var url = 'mongodb://localhost:27017/dvp-engagements';
 
 var database;
+var baseDb;
 MongoClient.connect(url, function (err, db) {
     assert.equal(null, err);
     console.log("Connected correctly to server");
     database = db.collection("Engagements");
+    baseDb=db;
 });
 
 var resetConnection = function () {
@@ -79,115 +81,7 @@ var updateLinks = function (sessionId, links, eng, res) {
 
 };
 
-var saveSessionId = function (sessionId) {
-
-    var now = moment(new Date());
-    var date = now.format("DDMMYYYY");
-    var databaseUrl = config.DB.Host + "/" + config.DB.Database,
-        collections = [date],
-        db = require("mongojs")(databaseUrl, collections);
-
-
-    db[date].findOne({sessionid: sessionId}, function (err, sid) {
-        if (sid) {
-            db[sessionId].save({
-                sessionid: sessionId,
-                create: now.format("DD-MM-YYYY-HH:mm:ss:SSS")
-            }, function (err, result) {
-                if (err) {
-                    logger.error('saveSessionId - [%s]', sessionId, err);
-                }
-            });
-        }
-    });
-};
-
-var saveEngagementdata = function (tenant, company, sessionId, req, res) {
-
-    res.setHeader('Content-Type', 'application/json');
-
-    var collection = database.collection(sessionId.toString());
-    var now = moment(new Date());
-    var date = now.format("DD-MM-YYYY-HH:mm:ss:SSS");
-    var jsonString;
-    // Insert some documents
-    collection.insertOne({
-            sessionid: sessionId,
-            engagementtype: req.body.EngagementType,
-            tenant: tenant,
-            company: company,
-            data: req.body.Data,
-            links: [],
-            create: date
-        }, function (err, result) {
-
-            if (!err) {
-                id = result._id;
-                logger.info('saveEngagement - [%s]', sessionId);
-                jsonString = messageFormatter.FormatMessage(undefined, "EXCEPTION", true, result);
-                res.end(jsonString);
-            }
-            else {
-                if (err.name == "MongoError") {
-                    resetConnection();
-                }
-                logger.error('saveEngagement - [%s]', sessionId, err);
-                jsonString = messageFormatter.FormatMessage(err, "EXCEPTION", false, undefined);
-                res.end(jsonString);
-            }
-        }
-    );
-
-
-    /*var databaseUrl = config.DB.Host + "/" + config.DB.Database,//example.com/mydb
-     collections = [sessionId],
-     db = require("mongojs")(databaseUrl, collections);
-     res.setHeader('Content-Type', 'application/json');
-     var jsonString;
-
-     var now = moment(new Date());
-     var date = now.format("DD-MM-YYYY-HH:mm:ss:SSS");
-     db[sessionId].save({
-     sessionid: sessionId,
-     engagementtype: req.body.EngagementType,
-     tenant: tenant,
-     company: company,
-     data: req.body.Data,
-     links: [],
-     create: date
-     }, function (err, eng) {
-     if (!err) {
-     id = eng._id;
-     logger.info('saveEngagement - [%s]', sessionId);
-     jsonString = messageFormatter.FormatMessage(undefined, "EXCEPTION", true, eng);
-     res.end(jsonString);
-     }
-     else {
-     logger.error('saveEngagement - [%s]', sessionId, err);
-     jsonString = messageFormatter.FormatMessage(err, "EXCEPTION", false, undefined);
-     res.end(jsonString);
-     }
-     })*/
-};
-
 exports.saveEngagement = function (tenant, company, req, res) {
-
-
-    /*var collection = database.collection("categories");
-
-     collection.insert({_id: "MongoDB", ancestors: ["Books", "Programming", "Databases"], parent: "Databases"})
-     collection.insert({_id: "dbm", ancestors: ["Books", "Programming", "Databases"], parent: "Databases"})
-     collection.insert({_id: "Databases", ancestors: ["Books", "Programming"], parent: "Programming"})
-     collection.insert({_id: "Languages", ancestors: ["Books", "Programming"], parent: "Programming"})
-     collection.insert({_id: "Programming", ancestors: ["Books"], parent: "Books"})
-     collection.insert({_id: "Books", ancestors: [], parent: null})
-
-     var tt = collection.findOne({_id: "MongoDB"}).ancestors
-
-     var twt = collection.findOne({_id: "MongoDB"}, function (er, r) {
-     r.ancestors;
-     })
-     */
 
     var items = [{tenant: tenant, company: company, sessionId: req.body.sessionId.toString(), req: req, res: res}];
     var task = [];
@@ -256,35 +150,35 @@ exports.saveEngagement = function (tenant, company, req, res) {
                 }
             });
         });
-        /*task.push(function () {
+        task.push(function () {
 
-         var now = moment(new Date());
-         var schm = now.format("DDMMYYYY");
-         var collection = database.collection(schm.toString());
-         var now = moment(new Date());
+            var now = moment(new Date());
+            var schm = now.format("DDMMYYYY");
+            var collection = baseDb.collection(schm.toString());
+            var now = moment(new Date());
 
-         collection.findOne({sessionid: item.sessionId.toString()}, function (err, result) {
-         if (!err) {
-         if (!result) {
-         collection.insertOne({
-         sessionid: item.sessionId.toString(),
-         create: now.format("DD-MM-YYYY-HH:mm:ss:SSS")
-         }, function (err, result) {
-         if (err) {
-         logger.error('saveSessionId - [%s]', item.sessionId, err);
-         }
-         });
-         }
-         }
-         else {
-         if (err.name == "MongoError") {
-         resetConnection();
-         }
-         }
+            collection.findOne({sessionid: item.sessionId.toString()}, function (err, result) {
+                    if (!err) {
+                        if (!result) {
+                            collection.insertOne({
+                                sessionid: item.sessionId.toString(),
+                                create: now.format("DD-MM-YYYY-HH:mm:ss:SSS")
+                            }, function (err, result) {
+                                if (err) {
+                                    logger.error('saveSessionId - [%s]', item.sessionId, err);
+                                }
+                            });
+                        }
+                    }
+                    else {
+                        if (err.name == "MongoError") {
+                            resetConnection();
+                        }
+                    }
 
-         }
-         );
-         });*/
+                }
+            );
+        });
     });
     async.parallel(task, null);
 };
@@ -323,7 +217,9 @@ exports.getEngagementsBySessionId = function (tenant, company, req, res) {
         if (!err) {
             if (eng) {
                 var ids = eng.links;
-                ids = ids.map(function(id) { return ObjectID(id); });
+                ids = ids.map(function (id) {
+                    return ObjectID(id);
+                });
 
                 database.find({_id: {$in: ids}}).toArray(function (err, anc) {
                     if (!err) {
@@ -361,8 +257,7 @@ exports.getAllAttachments = function (tenant, company, req, res) {
     var engagementId = req.params.engagementId.toString();
     res.setHeader('Content-Type', 'application/json');
     var jsonString;
-    if(ObjectID.isValid(engagementId))
-    {
+    if (ObjectID.isValid(engagementId)) {
         engagementId = new ObjectID(engagementId);
     }
 
@@ -370,7 +265,9 @@ exports.getAllAttachments = function (tenant, company, req, res) {
         if (!err) {
             if (eng) {
                 var ids = eng.ancestors;
-                ids = ids.map(function(id) {return ObjectID(id);});
+                ids = ids.map(function (id) {
+                    return ObjectID(id);
+                });
 
                 database.find({_id: {$in: ids}}).toArray(function (err, anc) {
                     if (!err) {
@@ -497,7 +394,7 @@ exports.getAllEngagementsByDate = function (tenant, company, req, res) {
         size = parseInt(req.params.Size),
         skip = page > 0 ? ((page - 1) * size) : 0;
 
-    var collection = database.collection(selectedDate.toString());
+    var collection = baseDb.collection(selectedDate.toString());
     res.setHeader('Content-Type', 'application/json');
     var jsonString;
 
